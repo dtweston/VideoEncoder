57322f341dae69e5b2b3bf32627439fcbc0b51df
Dave Weston <dweston@yammer-inc.com>
2015-03-11 15:20:03 -0700

    Initial Commit

---
+++ VideoEncoder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoEncoder
+//
+//  Created by Dave Weston on 3/11/15.
+//  Copyright (c) 2015 Binocracy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoEncoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoEncoder
+//
+//  Created by Dave Weston on 3/11/15.
+//  Copyright (c) 2015 Binocracy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
